be611381a3548fdd8625e39ee84b55b5fc098479
Tristan Grovender <46428421+tristangrovender@users.noreply.github.com>
2021-11-03 20:08:45 -0500

    Added some logs

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -12,6 +12,14 @@ const Login = props => {
   const [formIsValid, setFormIsValid] = useState(false);
 
   useEffect(() => {
+    console.log("EFFECT RUNNING");
+
+    return () => {
+      console.log("EFFECT CLEANUP");
+    };
+  }, []);
+
+  useEffect(() => {
     const identifier = setTimeout(() => {
       setFormIsValid(
         enteredEmail.includes("@") && enteredPassword.trim().length > 6
